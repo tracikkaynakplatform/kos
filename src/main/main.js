2b9b6c59db04ae990efec883058ae253f877924a
Muhammed Karamuk <mkaramuk2002@gmail.com>
2022-08-26 16:46:27 +0300

    main.js sadeleştirildi.

--- src/main/main.js
+++ src/main/main.js
@@ -1,8 +1,6 @@
 import { app, BrowserWindow, ipcMain } from 'electron';
 import { initApis } from './api';
 
-let win;
-
 const createWindow = () => {
 	const mainWindow = new BrowserWindow({
 		width: 1000,
@@ -11,22 +9,13 @@ const createWindow = () => {
 			preload: MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY,
 		},
 	});
-	win = mainWindow
 	mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
 	mainWindow.webContents.openDevTools();
 };
 
 if (require('electron-squirrel-startup'))
 	app.quit();
-app.whenReady().then(() => {
-	initApis();
-});
+app.whenReady().then(() => initApis());	
 app.on('ready', createWindow);
-app.on('activate', () => {
-	if (BrowserWindow.getAllWindows().length === 0)
-		createWindow();
-});
-app.on('window-all-closed', () => {
-	if (process.platform !== 'darwin')
-		app.quit();
-});
+app.on('activate', () => BrowserWindow.getAllWindows().length === 0 ? createWindow() : null);
+app.on('window-all-closed', () => process.platform !== 'darwin' ? app.quit() : null);
