c03fd893afa48de41abba01b7f151f30f34107c0
Muhammed Karamuk <mkaramuk2002@gmail.com>
2022-09-03 09:43:34 +0300

    Dizin kontrol fonksiyonu eklendi
    Bu fonksiyon sayesinde programın çalışması için gerekli dizinler tek bir noktadan kontrol edilerek yeniden oluşturulabiliyor.

---
+++ src/main/utils/dir-checker.js
@@ -0,0 +1,37 @@
+import { access, mkdir, constants } from "fs";
+import { cwd } from "process";
+
+// Programın çalışması için  gereken temel dizinler
+export const DIRS = {
+	config: "config",
+	bin: "bin",
+};
+
+/**
+ * Çalışma dizini altında istenen dizinin var olup
+ * olmadığını döndürür. Eğer dizin cwd() + dizin yolunda
+ * bulunmuyorsa oluşturmayı dener. Oluşturma işlemi
+ * başarılı ise yolu döndürür. Başarısız ise hata
+ * fırlatır.
+ *
+ * @param {string} dir - Yolu istenen dizin. {@var DIRS} içerisinden bir dizin olmalı.
+ * @throws {Error} - Olmayan dizini oluşturma sırasında bir hata ile karşılaşırsa fırlatır.
+ * @throws {Error} - {@param dir} isimli bir dizin {@var {DIRS}} içerisinde yoksa fırlatır.
+ * @returns {Promise<string>} - Dizinin yolunu döndürür.
+ */
+export default async function dirCheck(dir) {
+	dir = DIRS[dir];
+	if (!dir) throw new Error("Dizin bilgisi verilmedi: " + dir);
+	let path = `${cwd()}/${dir}`;
+	return new Promise((resolve, reject) => {
+		access(path, constants.F_OK, (err) => {
+			if (!err) resolve(path);
+			else {
+				mkdir(path, { recursive: true }, (err) => {
+					if (!err) resolve(path);
+					reject(err);
+				});
+			}
+		});
+	});
+}
