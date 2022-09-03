e56cd5e7bc1c98c14f30512c91bb658ec8e9149c
Muhammed Karamuk <mkaramuk2002@gmail.com>
2022-09-03 14:49:07 +0300

    Üst bar kaldırıldı

--- src/react-app/components/Sidebar.js
+++ src/react-app/components/Sidebar.js
@@ -5,19 +5,25 @@ import {
 	List,
 	ListItemButton,
 	Typography,
-} from '@mui/material';
-import {
-	Home as HomeIcon,
-	Storage as StorageIcon
-} from '@mui/icons-material';
+} from "@mui/material";
+import { Home as HomeIcon, Storage as StorageIcon } from "@mui/icons-material";
 import { sidebarWidth } from "../config";
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function SidebarListButton({ icon, to, label, ...props }) {
 	const navigate = useNavigate();
 	return (
-		<ListItemButton onClick={() => to ? navigate(to, { replace: true }) : null} sx={{ display: 'flex', flexDirection: 'row', gap: '20px', mt: '10px', mb: '10px' }}>
+		<ListItemButton
+			onClick={() => (to ? navigate(to, { replace: true }) : null)}
+			sx={{
+				display: "flex",
+				flexDirection: "row",
+				gap: "20px",
+				mt: "10px",
+				mb: "10px",
+			}}
+		>
 			{icon}
 			<Typography>{label}</Typography>
 		</ListItemButton>
@@ -26,19 +32,32 @@ function SidebarListButton({ icon, to, label, ...props }) {
 
 export function Sidebar(props) {
 	return (
-		<Drawer sx={{
-			width: sidebarWidth,
-			flexShrink: 0,
-			[`& .MuiDrawer-paper`]: { width: sidebarWidth, boxSizing: 'border-box' },
-			...props
-		}} variant='permanent'>
-			<Toolbar />
-			<Box sx={{ overflow: 'auto' }}>
+		<Drawer
+			sx={{
+				width: sidebarWidth,
+				flexShrink: 0,
+				[`& .MuiDrawer-paper`]: {
+					width: sidebarWidth,
+					boxSizing: "border-box",
+				},
+				...props,
+			}}
+			variant="permanent"
+		>
+			<Box sx={{ overflow: "auto" }}>
 				<List>
-					<SidebarListButton to='/main_window' icon={<HomeIcon />} label='Ana Sayfa' />
-					<SidebarListButton to='/clusters' icon={<StorageIcon />} label='Kümeler' />
+					<SidebarListButton
+						to="/main_window"
+						icon={<HomeIcon />}
+						label="Ana Sayfa"
+					/>
+					<SidebarListButton
+						to="/clusters"
+						icon={<StorageIcon />}
+						label="Kümeler"
+					/>
 				</List>
 			</Box>
 		</Drawer>
-	)
+	);
 }
--- src/react-app/layouts/DashboardLayout.js
+++ src/react-app/layouts/DashboardLayout.js
@@ -1,27 +1,22 @@
-import {
-	Box,
-	AppBar,
-	Toolbar,
-	Typography
-} from '@mui/material';
-import { SnackbarProvider } from 'notistack';
-import { Sidebar } from '../components/Sidebar';
-import React from 'react';
+import { Box, AppBar, Toolbar, Typography } from "@mui/material";
+import { SnackbarProvider } from "notistack";
+import { Sidebar } from "../components/Sidebar";
+import React from "react";
 
 export default function DashboardLayout(props) {
 	return (
 		<SnackbarProvider maxSnack={5}>
-			<Box sx={{ display: 'flex' }}>
-				<AppBar sx={{ zIndex: (theme) => theme.zIndex.drawer + 1, backgroundColor: '#023E8A' }} position="fixed">
-					<Toolbar>
-						<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-							Kubernetes Orkestrasyon Sistemi
-						</Typography>
-					</Toolbar>
-				</AppBar>
+			<Box sx={{ display: "flex" }}>
 				<Sidebar />
-				<Box id='dsa' sx={{ display: 'flex', flexDirection: 'column', flexGrow: 1, height: '90vh' }}>
-					<Toolbar /> 
+				<Box
+					id="dsa"
+					sx={{
+						display: "flex",
+						flexDirection: "column",
+						flexGrow: 1,
+						height: "90vh",
+					}}
+				>
 					{props.children}
 				</Box>
 			</Box>
