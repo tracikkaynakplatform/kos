9d07e91352d1727f66597835853eef3509bd2817
Muhammed Karamuk <mkaramuk2002@gmail.com>
2022-10-06 16:08:41 +0300

    AWS bilgilerini tutacak dosya eklendi

---
+++ src/react-app/providers/aws.js
@@ -0,0 +1,19 @@
+export const machineTypes = [
+	{
+		name: "t3.nano",
+	},
+	{
+		name: "t3.micro",
+	},
+	{
+		name: "t3.small",
+	},
+	{
+		name: "t3.medium",
+	},
+	{
+		name: "t3.large",
+	},
+];
+
+export const regions = ["eu-west-1"];
--- src/react-app/providers/provider-logos.js
+++ src/react-app/providers/provider-logos.js
@@ -3,7 +3,6 @@ import awsLogo from "../public/assets/images/logos/aws_32x32.png";
 import digitalOceanLogo from "../public/assets/images/logos/do_32x32.png";
 import gcpLogo from "../public/assets/images/logos/gcp_32x32.png";
 import dockerLogo from "../public/assets/images/logos/docker_32x32.png";
-import unknownLogo from "../public/assets/images/logos/unknown_32x32.png";
 
 export const providerLogos = {};
 
