bfe9ccd074512a6473806e18fab9ac5b9fc93191
Muhammed Karamuk <mkaramuk2002@gmail.com>
2022-10-23 08:43:45 +0300

    A temporary solution added for jest
    We use NODE_ENV environment variable to checking that are we in the test environment or not and set 'modules' field of @babel/preset-env preset.

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,17 @@
+const { env } = require("process");
+
 module.exports = {
-  presets: [
-    //TODO: cannot use import in tests, but browser code gives module exceptions
-    //  when below is disabled
-    //  eg: Uncaught ReferenceError: exports is not defined
-    // ['@babel/preset-env', {"targets": {"node":true}}],
-    ['@babel/preset-env', {"targets": {"node":true}, "modules": false}],
-  ]
+	presets: [
+		//TODO: cannot use import in tests, but browser code gives module exceptions
+		//  when below is disabled
+		//  eg: Uncaught ReferenceError: exports is not defined
+		// ['@babel/preset-env', {"targets": {"node":true}}],
+		[
+			"@babel/preset-env",
+			{
+				targets: { node: true },
+				modules: env.NODE_ENV === "test" ? undefined : false,
+			},
+		],
+	],
 };
--- src/test/main/utils/download-file.test.js
+++ src/test/main/utils/download-file.test.js
@@ -1,36 +1,34 @@
 import downloadFile from "../../../main/utils/download-file";
 import * as fs from "fs";
-import * as path from "path"
-
+import * as path from "path";
 
 // Working directory.
-const TESTDIR = './.test_tmp';
-const DOWNLOAD_LOCALPATH = path.join(TESTDIR, 'bootstrap-components.yaml');
-const DOWNLOAD_URL = 'https://github.com/kubernetes-sigs/cluster-api/releases/download/v1.2.4/bootstrap-components.yaml'
+const TESTDIR = "./.test_tmp";
+const DOWNLOAD_LOCALPATH = path.join(TESTDIR, "bootstrap-components.yaml");
+const DOWNLOAD_URL =
+	"https://github.com/kubernetes-sigs/cluster-api/releases/download/v1.2.4/bootstrap-components.yaml";
 
 // beforeAll(() => jest.setTimeout(90 * 1000))
 
 test(`creating directory ${TESTDIR} for downloadFile tests`, () => {
-  fs.rmSync(TESTDIR, { recursive: true, force: true });
-  fs.mkdirSync(TESTDIR);
-  // fs.existsSync(WD)
-  expect(fs.existsSync(TESTDIR)).toBeTruthy();
-  const stats = fs.lstatSync(TESTDIR);
-  expect(stats.isDirectory()).toBeTruthy();
-  expect(fs.readdirSync(TESTDIR).length).toBe(0);
+	fs.rmdirSync(TESTDIR, { recursive: true, force: true });
+	fs.mkdirSync(TESTDIR);
+	// fs.existsSync(WD)
+	expect(fs.existsSync(TESTDIR)).toBeTruthy();
+	const stats = fs.lstatSync(TESTDIR);
+	expect(stats.isDirectory()).toBeTruthy();
+	expect(fs.readdirSync(TESTDIR).length).toBe(0);
 });
 
 test(`downloading a file to ${DOWNLOAD_LOCALPATH} via downloadFile`, async () => {
-  expect(fs.existsSync(TESTDIR)).toBeTruthy();
-  expect(fs.existsSync(`${DOWNLOAD_LOCALPATH}`)).toBeFalsy();
+	expect(fs.existsSync(TESTDIR)).toBeTruthy();
+	expect(fs.existsSync(`${DOWNLOAD_LOCALPATH}`)).toBeFalsy();
 
-  const ret = await downloadFile(DOWNLOAD_URL, DOWNLOAD_LOCALPATH);
-  expect(fs.existsSync(`${DOWNLOAD_LOCALPATH}`)).toBeTruthy();
+	const ret = await downloadFile(DOWNLOAD_URL, DOWNLOAD_LOCALPATH);
+	expect(fs.existsSync(`${DOWNLOAD_LOCALPATH}`)).toBeTruthy();
 });
-
 
 test(`cleaning directory ${TESTDIR} for downloadFile tests`, () => {
-  fs.rmSync(TESTDIR, { recursive: true, force: true });
-  expect(fs.existsSync(TESTDIR)).toBeFalsy();
+	fs.rmdirSync(TESTDIR, { recursive: true, force: true });
+	expect(fs.existsSync(TESTDIR)).toBeFalsy();
 });
-
