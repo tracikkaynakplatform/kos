52b29c2b097175bac94c8d1f0d8f03674409527f
Muhammed Karamuk <mkaramuk2002@gmail.com>
2022-11-20 22:59:40 +0300

    Added input validation by using react-hook-form

--- src/react-app/components/CreateClusterWizard/StepAWSProviderEKSConfig.jsx
+++ src/react-app/components/CreateClusterWizard/StepAWSProviderEKSConfig.jsx
@@ -1,64 +1,134 @@
 import React from "react";
 import { eksVersions, machineTypes, regions } from "../../providers/aws";
 import { useWizard } from "../../hooks/useWizard";
-import StepInput from "../StepInput.jsx";
+import { Grid } from "@mui/material";
+import { useForm } from "react-hook-form";
+import StepWizardWrapper from "../StepWizardWrapper.jsx";
+import InputText from "../FormInputs/InputText.jsx";
+import InputSelect from "../FormInputs/InputSelect.jsx";
 
 export default function StepAWSProviderEKSConfig({ goToNamedStep, ...props }) {
+	const { handleSubmit, control } = useForm();
 	const wizard = useWizard();
 
 	return (
-		<StepInput
+		<StepWizardWrapper
 			onBackClick={() => {
 				goToNamedStep("selectAWSClusterType");
 			}}
-			onNextClick={async (fields) => {
+			onNextClick={handleSubmit(async (fields) => {
 				for (let field of Object.keys(fields))
 					await wizard.updateData(field, fields[field]);
 				wizard.updateData("type", "eks");
 				goToNamedStep("AWSCreateCluster");
-			}}
+			})}
 			title="AWS-EKS Küme Bilgileri"
 			text="Oluşturulacak EKS kümesinin detaylarını girin."
-			fields={{
-				clusterName: {
-					title: "Küme Adı",
-					type: "text",
-					size: 6,
-				},
-				kubVersion: {
-					title: "EKS Kubernetes Versiyonu",
-					type: "select",
-					items: eksVersions,
-					value: eksVersions[0],
-					size: 6,
-				},
-				workerCount: {
-					title: "Worker Makina Sayısı",
-					type: "number",
-					size: 6,
-				},
-				workerMachineType: {
-					title: "Worker Makina Tipi",
-					type: "select",
-					items: machineTypes.map((x) => x.name),
-					value: machineTypes.map((x) => x.name)[0],
-					size: 6,
-				},
-				sshKeyName: {
-					title: "SSH Anahtarı",
-					type: "text",
-					size: 6,
-				},
-				region: {
-					title: "Bölge",
-					type: "select",
-					items: regions,
-					value: regions[0],
-					size: 6,
-				},
-			}}
 			width={500}
 			{...props}
-		/>
+		>
+			<Grid container spacing={1}>
+				<Grid item xs={6}>
+					<InputText
+						name="clusterName"
+						control={control}
+						label="Küme adı"
+						rules={{
+							required: "Küme adı giriniz",
+							minLength: {
+								value: 3,
+								message: "Minimum uzunluk 3 karakter olmalı",
+							},
+						}}
+					/>
+				</Grid>
+				<Grid item xs={6}>
+					<InputSelect
+						name="kubVersion"
+						control={control}
+						label="Kubernetes versiyonu"
+						items={eksVersions}
+						rules={{
+							required: "Versiyon giriniz",
+							minLength: {
+								value: 1,
+								message: "Versiyon giriniz",
+							},
+						}}
+					/>
+				</Grid>
+				<Grid item xs={6}>
+					<InputText
+						name="masterCount"
+						control={control}
+						label="Control Plane adedi"
+						componentProps={{ type: "number" }}
+						rules={{
+							required: "Lütfen adet giriniz",
+							min: {
+								value: 1,
+								message: "Lütfen adet giriniz",
+							},
+						}}
+					/>
+				</Grid>
+				<Grid item xs={6}>
+					<InputText
+						name="workerCount"
+						control={control}
+						label="Worker adedi"
+						componentProps={{ type: "number" }}
+						rules={{
+							required: "Lütfen adet giriniz",
+							min: {
+								value: 1,
+								message: "Lütfen adet giriniz",
+							},
+						}}
+					/>
+				</Grid>
+				<Grid item xs={6}>
+					<InputText
+						name="sshKeyName"
+						control={control}
+						label="SSH anahtar adı"
+						rules={{
+							required: "SSH anahtar adını giriniz",
+						}}
+					/>
+				</Grid>
+
+				<Grid item xs={6}>
+					<InputSelect
+						name="workerMachineType"
+						control={control}
+						label="Worker makina tipi"
+						items={machineTypes.map((x) => x.name)}
+						rules={{
+							required: "Makina tipini giriniz",
+							minLength: {
+								value: 1,
+								message: "Makina tipini giriniz",
+							},
+						}}
+					/>
+				</Grid>
+				<Grid item xs={6}>
+					<InputSelect
+						name="region"
+						control={control}
+						label="Bölge"
+						items={regions}
+						rules={{
+							required: "Bölge giriniz",
+							minLength: {
+								value: 1,
+								message: "Bölge giriniz",
+							},
+						}}
+					/>
+				</Grid>
+			</Grid>
+		</StepWizardWrapper>
 	);
 }
