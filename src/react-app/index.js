cf1e8fac17914317741c4690dbfc9fbd8801d62e
Muhammed Karamuk <mkaramuk2002@gmail.com>
2022-11-26 09:28:41 +0300

    Tailwindcss added
    In the future we won't need Material UI anymore. MUI has a great UX but that makes its a little bit slow. We can recreate MUI components by using tailwindcss. It is easy-to-use and more performance that MUI.

--- src/react-app/index.js
+++ src/react-app/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
+import "./styles/index.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { SnackbarProvider } from "notistack";
 import { ModalProvider } from "./hooks/useModal.js";
