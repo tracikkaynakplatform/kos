ca38b1833e313062ad5a2a0a146d2eca186c05e7
Muhammed Karamuk <mkaramuk2002@gmail.com>
2022-12-12 20:39:17 +0300

    Packaging bug fixed
    This bug was due to trying to get function name by function.prototype.name but when webpack minify the code, names of the functions were missing.

--- src/main/api/env.js
+++ src/main/api/env.js
@@ -1,5 +1,7 @@
+import { exportHelper } from "./exportHelper";
+
 export async function getEnv(name) {
 	return process.env[name];
 }
 
-export default [getEnv];
+export default [exportHelper("getEnv", getEnv)];
