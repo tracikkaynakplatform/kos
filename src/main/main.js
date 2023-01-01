cb4694075a29ea492cdbc490a56ed562c021c926
Muhammed Hayseme Karamuk <mkaramuk2002@gmail.com>
2023-01-01 16:48:34 +0300

    Added tight click menu for input elements with electron-context-menu.

--- src/main/main.js
+++ src/main/main.js
@@ -1,4 +1,5 @@
-import { app, BrowserWindow, Menu} from "electron";
+import { app, BrowserWindow, Menu } from "electron";
+import contextMenu from "electron-context-menu";
 import { initApis } from "./api";
 
 const createWindow = () => {
@@ -9,38 +10,51 @@ const createWindow = () => {
 			preload: MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY,
 		},
 	});
+	contextMenu({
+		window: mainWindow,
+		labels: {
+			copy: "Kopyala",
+			paste: "Yapıştır",
+			cut: "Kes",
+		},
+		showSelectAll: false,
+		showSearchWithGoogle: false,
+		showInspectElement: false,
+	});
 	mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
 	if (process.env.NODE_ENV === "development")
 		mainWindow.webContents.openDevTools();
-
 	const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
-	 Menu.setApplicationMenu(mainMenu);
+	Menu.setApplicationMenu(mainMenu);
 };
 
 const mainMenuTemplate = [
 	{
-	  label : "Menu",
-	  submenu : [
-		{
-		  label : "Çıkış",
-		  accelerator : process.platform == "darwin" ? "Command+Q": "Ctrl+Q",
-		  role : "quit"
-		}
-	  ]
-	}
-  ]
-   
-  if (process.platform == "darwin"){
+		label: "Menu",
+		submenu: [
+			{
+				label: "Çıkış",
+				accelerator:
+					process.platform == "darwin" ? "Command+Q" : "Ctrl+Q",
+				role: "quit",
+			},
+		],
+	},
+];
+
+if (process.platform == "darwin") {
 	mainMenuTemplate.unshift({
-	  label :app.getName(),
-	  role : "TODO"
-	})
-  }
-
-
+		label: app.getName(),
+		role: "TODO",
+	});
+}
 
 if (require("electron-squirrel-startup")) app.quit();
-app.whenReady().then(() => initApis()); // API'lerin yüklenmesi ve kullanıma hazır hale gelmesi.
+
+app.whenReady().then(() => {
+	initApis();
+});
+
 app.on("ready", createWindow);
 app.on("activate", () =>
 	BrowserWindow.getAllWindows().length === 0 ? createWindow() : null
