5dfb71175681f8e0f066157c45fa2100f6a543bf
Muhammed Hayseme Karamuk <mkaramuk2002@gmail.com>
2023-01-31 21:15:54 +0300

    Wrong imports fixed, code formatted.

--- src/test/main/services/base/platform.test.js
+++ src/test/main/services/base/platform.test.js
@@ -1,7 +1,9 @@
-import { platform } from "../../../../main/services/base/platform";
+import { platform } from "../../../../main/services/base/Platform";
 
 test(`testing platform singleton/immutable`, () => {
-  expect(platform.arch.length > 0).toBeTruthy();
-  expect(platform.osFamily.length > 0).toBeTruthy();
-  expect(() => {platform.arch = 'unexpected-value-from-test!'}).toThrow();
+	expect(platform.arch.length > 0).toBeTruthy();
+	expect(platform.osFamily.length > 0).toBeTruthy();
+	expect(() => {
+		platform.arch = "unexpected-value-from-test!";
+	}).toThrow();
 });
