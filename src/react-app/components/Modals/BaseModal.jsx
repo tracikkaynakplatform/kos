d3cdda081a618fa493f51dc92f00be00b16cc399
Muhammed Hayseme Karamuk <mkaramuk2002@gmail.com>
2023-02-01 17:42:36 +0300

    Added width prop to BaseModal and Prettier.

--- src/react-app/components/Modals/BaseModal.jsx
+++ src/react-app/components/Modals/BaseModal.jsx
@@ -1,31 +1,34 @@
 import React, { forwardRef } from "react";
 import { Box, Modal } from "@mui/material";
 
-export const BaseModal = forwardRef(({ open, onClose, children }, ref) => (
-	<Modal ref={ref} open={open} onClose={onClose}>
-		<Box
-			sx={{
-				display: "flex",
-				alignItems: "center",
-				justifyContent: "space-between",
-				flexDirection: "column",
-				position: "absolute",
-				gap: "10px",
-				top: "50%",
-				left: "50%",
-				transform: "translate(-50%, -50%)",
-				boxShadow: 24,
-				backgroundColor: "white",
-				p: 3,
-				borderRadius: "20px",
-				":focus-visible": {
-					outline: "none",
-				},
-			}}
-		>
-			{children}
-		</Box>
-	</Modal>
-));
+export const BaseModal = forwardRef(
+	({ open, onClose, children, width }, ref) => (
+		<Modal ref={ref} open={open} onClose={onClose}>
+			<Box
+				sx={{
+					display: "flex",
+					alignItems: "center",
+					justifyContent: "space-between",
+					flexDirection: "column",
+					position: "absolute",
+					gap: "10px",
+					top: "50%",
+					left: "50%",
+					transform: "translate(-50%, -50%)",
+					boxShadow: 24,
+					backgroundColor: "white",
+					p: 3,
+					borderRadius: "20px",
+					":focus-visible": {
+						outline: "none",
+					},
+					maxWidth: width,
+				}}
+			>
+				{children}
+			</Box>
+		</Modal>
+	)
+);
 
 export default BaseModal;
